b1b39e0bfd39336e1386050350f29922f39644e4
chadoma <chadoma15@gmail.com>
2020-09-03 07:02:48 +0900

    redo

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-//全てのTodoListItem
-data = {
-  allItems: [],
-}
-
+//全てのTodoList
+const todos = [];
 
 elements = {
-  itemInput: document.querySelector('.add_item_input'),
+  addItemInput: document.querySelector('.add_item_input'),
   addTaskBtn: document.querySelector('.add_item_btn'),
   todoItems: document.querySelector('.todo_table_items'),
   todoList: document.querySelector('.todo_list'),
-  displayNone: document.querySelector('.none'),
+  todoTable: document.querySelector('.todo_table'),
 }
 
 //addTaskBtnを押すと発火
 elements.addTaskBtn.addEventListener('click', (e) => {
-  e.preventDefault();
 
 //todolistを作成する関数
   addTodoItems();
@@ -28,36 +24,17 @@ elements.addTaskBtn.addEventListener('click', (e) => {
 
 //input入力をresetする関数
 const resetInput = () => {
-  elements.itemInput.value = '';
+  elements.addItemInput.value = '';
 }
 
 
 //todolistを作成する関数
 const addTodoItems = () => {
-  let ids, newTodoItem;
-  // create ids
-  //[1,2,3] 次のIDは４
-  //[1,3,4,6] 次のIDは7一度DELETEした番号は使えない様にした。
-  //ids = 最後のid+1とする
-
-  if (data.allItems.length > 0) {
-    ids = data.allItems[data.allItems.length - 1].id + 1;
-  } else {
-    ids = 0;
-  }
-  //空白入力を除去、newTodoItemとして、allItemsにpush
-  if (elements.itemInput.value.trim()) {
-    newTodoItem = {
-      id: ids,
-      desc: elements.itemInput.value.trim(),
-      state: false,
-    }
-    data.allItems.push(newTodoItem);
-    elements.displayNone.style.display = 'none';
-    //空白入力や、入力しないと警告を出す.
-  } else {
-    elements.displayNone.style.display = 'block';
-  }
+  let todo = elements.addItemInput.value.trim();
+  if (todo) {
+    todos.push(todo);
+  }else
+    console.log('aaaaa');
 }
 
 //Htmlにtodolistを表示する関数
@@ -68,29 +45,44 @@ const showTodoItems = () => {
     elements.todoItems.removeChild(elements.todoItems.firstChild);
   }
 
+  todos.forEach((todo, index)=>{
+    let state = '作業中';
+    let table = elements.todoItems;
+    let newRow = table.insertRow();
+    //各々セルに対する、データを作成
+    let newCell = newRow.insertCell();
+    let newText = document.createTextNode(index + 1);
+    newCell.appendChild(newText);
 
-  data.allItems.forEach((item, index) => {
-    html = `
-    <tr class="item-${ index }">
-    <td>%id%</td>
-    <td>%description%</td>
-    <td><button>%state%</button></td>
-    <td><button>削除</button></td>
-   </tr> 
-  `;
-    newHtml = html.replace('%id%', item.id);
-    newHtml = newHtml.replace('%description%', item.desc);
-    //taskが作業中か？否か。
-    if (!item.state) {
-      newHtml = newHtml.replace('%state%', '作業中');
-    } else {
-      newHtml = newHtml.replace('%state%', '完了');
+    newCell = newRow.insertCell();
+    newText = document.createTextNode(todo);
+    newCell.appendChild(newText);
 
-    }
-    document.querySelector('.todo_table_items').insertAdjacentHTML('beforeend', newHtml);
+    const stateButton = document.createElement('button');
+    stateButton.textContent = state;
+    newCell = newRow.insertCell();
+    newCell.appendChild(stateButton);
 
-  })
+    //todolistを消すbuttonを作成
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = '削除';
 
+    newCell = newRow.insertCell();
+    newCell.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', ()=>{
+      //todoListを消す関数
+      // deleteTodoItem(index);
+    });
+
+  });
 
 }
 
+// //todoItemを消す関数
+// const deleteTodoItem = (index)=>{
+//   todos.splice(index, 1);
+//   showTodoItems();
+// }
+//
+//
