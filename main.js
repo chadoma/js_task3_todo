e77c361f6fd680996c966491767e257a72501e30
chadoma <chadoma15@gmail.com>
2020-09-08 06:16:18 +0900

    i

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ elements = {
   todoItems: document.querySelector('.todo_table_items'),
   todoList: document.querySelector('.todo_list'),
   todoTable: document.querySelector('.todo_table')
-}
+};
 
 //addTaskBtnを押すと発火
 elements.addTaskBtn.addEventListener('click', () => {
 
 //todolistを作成する関数
-  addTodoItems();
+  createTodoItems();
 //htmlにtodoListを表示する関数
   showTodoItems();
 //input入力をresetする関数
@@ -25,19 +25,19 @@ elements.addTaskBtn.addEventListener('click', () => {
 //input入力をresetする関数
 const resetInput = () => {
   elements.addItemInput.value = '';
-}
+};
 
 
 //todolistを作成する関数
-const addTodoItems = () => {
+const createTodoItems = () => {
   const todo = elements.addItemInput.value.trim();
   if (todo) {
     todos.push({
       content: todo,
-      state: '作業中'
-    })
+      state: false
+    });
   }
-}
+};
 //Htmlにtodos[]を表示する関数
 const showTodoItems = () => {
 
@@ -60,14 +60,16 @@ const showTodoItems = () => {
     newCell.appendChild(newText);
 
     const stateButton = document.createElement('button');
-    stateButton.textContent = todo.state;
+    stateButton.addEventListener('click', () => {
+      toggleButton(stateButton, todo.state);
+    });
+    stateButton.textContent = todo.state ? '完了' : '作業中';
     newCell = newRow.insertCell();
     newCell.appendChild(stateButton);
 
     //todolistを消すbuttonを作成
     const deleteButton = document.createElement("button");
     deleteButton.textContent = '削除';
-
     newCell = newRow.insertCell();
     newCell.appendChild(deleteButton);
 
@@ -78,12 +80,18 @@ const showTodoItems = () => {
 
   });
 
-}
+};
 
 // todoItemを消す関数
-const deleteTodoItem = (index)=>{
+const deleteTodoItem = (index) => {
   todos.splice(index, 1);
   showTodoItems();
-}
+};
+
+const toggleButton = (button, state) => {
+  state = !state;
+  console.log(state);
+  button.textContent = state;
+};
 
 
